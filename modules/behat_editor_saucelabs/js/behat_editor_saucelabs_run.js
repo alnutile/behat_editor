df1257f2551e8115a3aaf1a08266a3ca0338c564
alnutile <alfrednutile@gmail.com>
2013-09-26 09:08:20 -0400

    Starting to work on the json interface to get the job info before and after submission so we can see that status and get data back

--- modules/behat_editor_saucelabs/js/behat_editor_saucelabs_run.js
+++ modules/behat_editor_saucelabs/js/behat_editor_saucelabs_run.js
@@ -5,12 +5,18 @@
 
             $('a.sauce').click(function(e){
                 e.preventDefault();
-                var method = 'view-mode';
-                var url = $(this).attr('href');
-                $.post(url, function(data){
-                    console.log(data);
-                    Drupal.behat_editor.renderMessage(data);
-                }, "json");
+                if(!$(this).hasClass('disabled')) {
+                    var method = 'view-mode';
+                    var url = $(this).attr('href');
+                    $.get('admin/behat/saucelabs/jobs', function(data){
+                        //Start looking for new jobs
+                        console.log(data);
+                    });
+//                    $.post(url, function(data){
+//                        console.log(data);
+//                        Drupal.behat_editor.renderMessage(data);
+//                    }, "json");
+                }
             });
         }
     };
