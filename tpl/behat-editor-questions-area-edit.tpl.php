d1da189a96aa00d8dda77db26110c22c54fca35c
alnutile <alfrednutile@gmail.com>
2013-10-09 06:38:24 -0400

    Lost some icons in the theme upgrade

--- tpl/behat-editor-questions-area-edit.tpl.php
+++ tpl/behat-editor-questions-area-edit.tpl.php
@@ -11,11 +11,11 @@
 <ul class="hidden example-test scenario sortable ui-sortable">
     <li class="tags"><span class='tag'>@example</span></li>
     <li class="feature">Feature: Example Test for WikiPedia</li>
-    <li class="name"><i class="icon-move"></i> Scenario: WikiPedia</li>
-    <li class="url"><i class="icon-move"></i> Given I am on "http://en.wikipedia.org/wiki/Main_Page"</li>
-    <li class="form_field_and_fill_in"><i class="icon-move"> </i>And I fill in "search" with "Behavior Driven Development" <i class="remove icon-remove-circle"></i></li>
-    <li class="click_on_type"><i class="icon-move"> </i>And I press "searchButton" <i class="remove icon-remove-circle"></i></li>
-    <li class="should_see"><i class="icon-move"> </i>Then I should see "agile software development" <i class="remove icon-remove-circle"></i></li>
-    <li class="click_on_type"><i class="icon-move"> </i>And I follow "Donate to Wikipedia" <i class="remove icon-remove-circle"></i></li>
-    <li class="should_see"><i class="icon-move"> </i>Then I should see "Thanks" <i class="remove icon-remove-circle"></i></li>
+    <li class="name"><i class="glyphicon glyphicon-move"></i> Scenario: WikiPedia</li>
+    <li class="url"><i class="glyphicon glyphicon-move"></i> Given I am on "http://en.wikipedia.org/wiki/Main_Page"</li>
+    <li class="form_field_and_fill_in"><i class="glyphicon glyphicon-move"> </i>And I fill in "search" with "Behavior Driven Development" <i class="remove glyphicon glyphicon-remove-circle"></i></li>
+    <li class="click_on_type"><i class="glyphicon glyphicon-move"> </i>And I press "searchButton" <i class="remove glyphicon glyphicon-remove-circle"></i></li>
+    <li class="should_see"><i class="glyphicon glyphicon-move"> </i>Then I should see "agile software development" <i class="remove glyphicon glyphicon-remove-circle"></i></li>
+    <li class="click_on_type"><i class="glyphicon glyphicon-move"> </i>And I follow "Donate to Wikipedia" <i class="remove glyphicon glyphicon-remove-circle"></i></li>
+    <li class="should_see"><i class="glyphicon glyphicon-move"> </i>Then I should see "Thanks" <i class="remove glyphicon glyphicon-remove-circle"></i></li>
 </ul>
