6f68cf6371f41839a8404cdda7b584c501f6b357
alnutile <alfrednutile@gmail.com>
2013-10-11 11:44:10 -0400

    Run is working issue #44

--- js/behat_editor_view.js
+++ js/behat_editor_view.js
@@ -4,15 +4,16 @@
         attach: function (context) {
 
             $('a.run').click(function(e){
+                var token = Drupal.behat_editor.get_token();
+                console.log(token);
                 e.preventDefault();
                 var method = 'view-mode';
                 var url = $(this).attr('href');
                 var parameters = {
                     "method": method
                 };
-                $.post(url, parameters, function(data){
-                    Drupal.behat_editor.renderMessage(data);
-                }, "json");
+                var data = Drupal.behat_editor.action('POST', token, parameters, url);
+                Drupal.behat_editor.renderMessage(data);
             });
         }
     };
