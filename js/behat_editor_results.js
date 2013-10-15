841735e3a7241fff26bfcd5b26c1e82e5491914b
alnutile <alfrednutile@gmail.com>
2013-10-15 07:38:22 -0400

    Working on results api to show the tests results

--- js/behat_editor_results.js
+++ js/behat_editor_results.js
@@ -3,14 +3,22 @@
 
         attach: function (context) {
             var token = Drupal.behat_editor.get_token();
-            var url = '/behat_editor_v1/behat_editor_actions/feature/results/behat_editor/wikipedia';
+            var filename = $('input[name=filename]').val();
+            var module = $('input[name=module]').val();
+            var url = '/behat_editor_v1/behat_editor_actions/feature/results/' + module + '/' + filename;
             var parameters = {};
             var results = Drupal.behat_editor.action('POST', token, parameters, url);
             if(results['error'] === 0) {
-                $('#past-results-table').dataTable(
+                var table = new Array();
+                var rows = results['data'];
+                $.each(rows, function(key, value){
+                    var date = new Date(value["created"]*1000);
+                    table[key] = [value["filename"], value["module"], value["duration"], date.format('Y-m-d H:i')]
+                });
 
+                $('#past-results-table').dataTable(
                     {
-                        "aaData": results['data'],
+                        "aaData": table,
                         "aoColumns": [
                             {"sTitle": "Filename"},
                             {"sTitle": "Module"},
