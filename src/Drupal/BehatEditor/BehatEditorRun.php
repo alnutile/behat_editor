f3d3959b5ad86af66babae41af1e60c31136124c
alnutile <alfrednutile@gmail.com>
2013-10-15 12:15:36 -0400

    Refactor the Drupal.action(s) method. I want to keep this DRY but I needed to set Globals on page load to false to not trigger some ajaxStart callbacks. I also needed to run callbacks after the request

--- src/Drupal/BehatEditor/BehatEditorRun.php
+++ src/Drupal/BehatEditor/BehatEditorRun.php
@@ -117,13 +117,12 @@ class BehatEditorRun {
         } else {
             $tags = "--tags '~@javascript'";
         }
-        //@todo come back and just use exec output and response return features.
-        //  Could save from writing to the drive
-        exec("cd $this->behat_path && ./bin/behat --config=\"$this->yml_path\" --no-paths $tags $this->absolute_file_path", $output);
+        exec("cd $this->behat_path && ./bin/behat --config=\"$this->yml_path\" --no-paths $tags $this->absolute_file_path", $output, $return_var);
         $this->file_array = $output;
-        //@todo why did I not go with exec return_var
+        //Just getting behat errors
+        //  return_var is the output of the test
         $response = is_array($output) ? 0 : 1;
-        self::saveResults($output, $response);
+        self::saveResults($output, $return_var);
         return array('response' => $response, 'output_file' => $this->output_file, 'output_array' => $output);
     }
 
