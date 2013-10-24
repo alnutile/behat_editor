d67af7d2aad3462337cbb131f89472986dbe53ae
alnutile <alfrednutile@gmail.com>
2013-10-24 12:14:14 -0400

    exception if file not saved

--- src/Drupal/BehatEditor/File.php
+++ src/Drupal/BehatEditor/File.php
@@ -236,8 +236,8 @@ class File {
         $path = file_build_uri("/{$folder}/");
         $response = file_unmanaged_save_data($this->feature, $path . '/' . $this->filename, $replace = FILE_EXISTS_REPLACE);
         if($response == FALSE) {
-            watchdog('behat_editor', "File could not be made.", $variables = array(), $severity = WATCHDOG_ERROR, $link = NULL);
-            $output = array('message' => "Error file could not be save", 'file' => $response, 'error' => '1');
+            $message = t('The file could not be saved !file', array('!file' => $path . '/' . $this->filename));
+            throw new \RuntimeException($message);
         } else {
             $file_uri = $response;
             $file_url = l('click here', file_create_url($response), array('attributes' => array('target' => '_blank', 'id' => array('test-file'))));
