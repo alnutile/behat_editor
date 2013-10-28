463b4199b8738b1a7f6f3bc26b5394d265b071aa
alnutile <alfrednutile@gmail.com>
2013-10-28 09:10:02 -0400

    Clean up some repetative code

--- run_tests.php
+++ run_tests.php
@@ -5,6 +5,8 @@
  * your project and type at the command line
  * ln -s ../../run_behat_tests.php .git/hooks/pre-commit
  * Mac users can get sound by using the say command around line 20.
+ * Then with every commit the tests will be run.
+ *
  * 
 */
 
@@ -12,6 +14,7 @@ define("WORKING_PATH", '/Users/alfrednutile/Drupal/pfizertestinstall/sites/all/m
 define("RUN_TEST", 'full_test.feature');
 chdir(WORKING_PATH);
 exec('git pull origin development', $output, $return_var);
+exec("drush cc all");
 exec("drush br behat_editor " .RUN_TEST. " 1 0 0", $output, $return_var);
 if ( !$return_var ) {
 	$message = "Failed Tests " . RUN_TEST;
