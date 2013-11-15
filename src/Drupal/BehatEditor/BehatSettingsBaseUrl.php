4ce634f3249a23dd503d6d27e7c1bf8b845dc544
alnutile <alfrednutile@gmail.com>
2013-11-15 11:22:59 -0500

    Add in place

---
+++ src/Drupal/BehatEditor/BehatSettingsBaseUrl.php
@@ -0,0 +1,90 @@
+<?php
+/**
+ * @file
+ *   Get settings for user and groups
+ */
+namespace Drupal\BehatEditor;
+
+
+class BehatSettingsBaseUrl {
+    public $userObject;
+    public $uid;
+    public $groups;
+
+    function __construct(){
+        $this->fields = self::fields();
+    }
+
+    function getUsersSettings($uid) {
+        $this->uid = $uid;
+
+    }
+
+    function getGroupSettingsForUser($uid, array $groups) {
+
+    }
+
+    static function userObject() {
+
+    }
+
+
+    static function getFields() {
+        return self::fields();
+    }
+
+    static public function getSettingsByUID($uid) {
+        $query = db_select('behat_editor_base_url_settings', 'b');
+        $query->fields('b');
+        $query->condition('b.uid', "$uid", '=');
+        $query->orderBy('b.sid', 'DESC');
+        $result = $query->execute();
+        $rows = array();
+        if ($result) {
+            foreach ($result as $record) {
+                $rows[] = (array) $record;
+            }
+        }
+        return array('results' => $rows, 'error' => 0);
+    }
+
+    public function insert() {
+        $insert = db_insert('behat_editor_base_url_settings')->fields($this->fields)->execute();
+        if($this->fields['default_url'] == 1) {
+            self::resetDefault($insert);
+        }
+        return $insert;
+    }
+
+    private function resetDefault($insert) {
+        //Select all for this user and group = id in $this->fields['gid']
+        // set default 0 unless it = this $insert sid
+        $uid = $this->fields['uid'];
+        $gid = $this->fields['gid'];
+        $sid = $insert;
+
+        $query = db_update('behat_editor_base_url_settings');
+        $query->fields(array('default_url' => 0));
+        if($gid != 0) {
+            $query->condition('gid', "$gid", '=');
+        } else {
+            $query->condition('uid', "$uid", '=');
+        }
+        $query->condition('sid', $sid, '!=');
+        $query->execute();
+    }
+
+
+    private function fields() {
+        global $user;
+        $fields['uid'] = $user->uid;
+        $fields['gid'] = 0;
+        $fields['base_url'] = NULL;
+        $fields['default_url'] = 0;
+        $fields['active'] = 1;
+        $fields['nice_name'] = NULL;
+
+        return $fields;
+    }
+
+}
